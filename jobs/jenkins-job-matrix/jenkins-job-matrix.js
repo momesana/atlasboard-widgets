bbefa7558007f58e3e0b76189a6effe7155a1893
Mehdi Salem <salem@axivion.com>
2018-12-14 18:03:35 +0100

    performance optimization for job-matrix

--- jobs/jenkins-job-matrix/jenkins-job-matrix.js
+++ jobs/jenkins-job-matrix/jenkins-job-matrix.js
@@ -1,63 +1,17 @@
-/**
- * Job: jenkins-job-matrix
- *
- * Expected configuration:
- *
- * ## PLEASE ADD AN EXAMPLE CONFIGURATION FOR YOUR JOB HERE
- * { 
- *   myconfigKey : [ 
- *     { serverUrl : 'localhost' } 
- *   ]
- * }
- */
-
 const axios = require('axios');
 
-const getRunData = async function (username, password, url) {
+const orderedStates = [undefined, 'success', 'unstable', 'failure'];
 
-	const response = await axios({
-		url: `${url}/api/json`,
-		auth: {
-			username,
-			password
-		},
-		headers: {
-			Accept: 'application/json',
-		}
-	});
+const isMoreSevere = (first, second) => orderedStates.indexOf(first) - orderedStates.indexOf(second) < 0;
 
-	const { result, builtOn, culprits } = response.data;
+const extractRunData = ({ result, builtOn, culprits }) => ({
+	label: (builtOn.includes('docker') ? 'linux' : builtOn), // Fixme: use a mapping to rename, merge items
+	value: result.toLowerCase(),
+	culprits: culprits.map(({ id }) => id)
+});
 
-	return {
-		label: (builtOn.includes('docker') ? 'linux' : builtOn), // Fixme: use a mapping to rename, merge items
-		value: result.toLowerCase(),
-		culprits: culprits.map(({ fullName }) => fullName)
-	};
-};
-
-const getBuildData = async function (username, password, url) {
-
-	const response = await axios({
-		url: `${url}/api/json`,
-		auth: {
-			username,
-			password
-		},
-		headers: {
-			Accept: 'application/json',
-		}
-	});
-
-	const results = await Promise.all(
-		response.data.runs.map(({ url }) => getRunData(username, password, url))
-	);
-
-	const orderedStates = [undefined, 'success', 'unstable', 'failure'];
-
-	const isMoreSevere = (first, second) =>
-		orderedStates.indexOf(first) - orderedStates.indexOf(second) < 0;
-
-	return results.reduce((acc, { label, value, culprits }) => {
+const getBuildResult = runData => {
+	return runData.reduce((acc, { label, value, culprits }) => {
 		if (isMoreSevere(acc.buildResult[label], value)) {
 			acc.buildResult[label] = value;
 		}
@@ -82,9 +36,8 @@ module.exports = {
 
 			const { username, accessToken: password } = globalAuth[authName];
 			const { numberOfItems, baseUrl, widgetTitle, matrixJob } = config;
-
 			const response = await axios({
-				url: `${baseUrl}/job/${matrixJob}/api/json`,
+				url: `${baseUrl}/job/${matrixJob}/api/json?tree=builds[runs[builtOn,result,id,fullDisplayName,culprits[id]]]{,${numberOfItems}}`,
 				auth: {
 					username,
 					password
@@ -94,22 +47,24 @@ module.exports = {
 				}
 			});
 
-			const urls = response.data.builds
-				.slice(0, numberOfItems)
-				.map(build => build.url);
+			const {jobResults, culprits} = response.data.builds
+				.map(({runs}) => runs.map(extractRunData))
+				.map(getBuildResult)
+				.reduce((acc, { buildResult, culprits }) => {
+					Object.entries(buildResult).forEach(([key, value]) => {
+						acc.jobResults[key] = acc.jobResults[key] ? [...acc.jobResults[key], value] : [value];
+						acc.culprits.add(...culprits);
+					});
+					return acc;
+				}, { jobResults: {}, culprits: new Set() });
 
-			const buildResults = await Promise
-				.all(urls.map(url => getBuildData(username, password, url)));
-
-			const { jobResults, culprits } = buildResults.reduce((acc, { buildResult, culprits }) => {
-				Object.entries(buildResult).forEach(([key, value]) => {
-					acc.jobResults[key] = acc.jobResults[key] ? [...acc.jobResults[key], value] : [value];
-					acc.culprits.add(...culprits);
-				});
-				return acc;
-			}, { jobResults: {}, culprits: new Set() });
-
-			jobCallback(null, { jobConfig: config, title: widgetTitle, subtitle: matrixJob, jobResults, culprits: [...culprits] });
+			jobCallback(null, {
+				jobConfig: config,
+				title: widgetTitle,
+				subtitle: matrixJob,
+				jobResults,
+				culprits: [...culprits]
+			});
 		} catch (e) {
 			console.error(e);
 			jobCallback(e.message);

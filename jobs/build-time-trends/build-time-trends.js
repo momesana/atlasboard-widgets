d9a525bb86201aaca9947a7f3084b061dceab342
Mehdi Salem <salem@axivion.com>
2018-12-14 20:02:07 +0100

    retrieve build times from jenkins

--- jobs/build-time-trends/build-time-trends.js
+++ jobs/build-time-trends/build-time-trends.js
@@ -1,95 +1,45 @@
-/**
- * Job: build-time-trends
- *
- * Expected configuration:
- *
- * ## PLEASE ADD AN EXAMPLE CONFIGURATION FOR YOUR JOB HERE
- * { 
- *   myconfigKey : [ 
- *     { serverUrl : 'localhost' } 
- *   ]
- * }
- */
+const axios = require('axios');
 
 module.exports = {
 
-  /**
-   * Executed on job initialisation (only once)
-   * @param config
-   * @param dependencies
-   */
-  onInit: function (config, dependencies) {
-
-    /*
-    This is a good place for initialisation logic, like registering routes in express:
-
-    dependencies.logger.info('adding routes...');
-    dependencies.app.route("/jobs/mycustomroute")
-        .get(function (req, res) {
-          res.end('So something useful here');
-        });
-    */
+  onInit(config, dependencies) {
   },
 
-  /**
-   * Executed every interval
-   * @param config
-   * @param dependencies
-   * @param jobCallback
-   */
-  onRun: function (config, dependencies, jobCallback) {
+  onRun: async function (config, dependencies, jobCallback) {
+    try {
+      const { globalAuth, authName = 'jenkins' } = config;
 
-    /*
-     1. USE OF JOB DEPENDENCIES
+      if (!globalAuth || !globalAuth[authName] ||
+          !globalAuth[authName].accessToken || !globalAuth[authName].username) {
+        return jobCallback('missing Jenkins credentials');
+      }
 
-     You can use a few handy dependencies in your job:
+      const { username, accessToken: password } = globalAuth[authName];
+      const { numberOfItems, baseUrl, job, descriptionFilter } = config;
+      const response = await axios({
+        url: `${baseUrl}/job/${job}/api/json?tree=builds[description,duration]`,
+        auth: {
+          username,
+          password
+        },
+        headers: {
+          Accept: 'application/json',
+        }
+      });
 
-     - dependencies.easyRequest : a wrapper on top of the "request" module
-     - dependencies.request : the popular http request module itself
-     - dependencies.logger : atlasboard logger interface
+      const buildTimes = response.data.builds
+          .filter(({ description }) => description && description.includes(descriptionFilter))
+          .map(({duration}) => parseInt(duration))
+          .slice(0, numberOfItems);
 
-     Check them all out: https://bitbucket.org/atlassian/atlasboard/raw/master/lib/job-dependencies/?at=master
-
-     */
-
-    var logger = dependencies.logger;
-
-    /*
-
-     2. CONFIGURATION CHECK
-
-     You probably want to check that the right configuration has been passed to the job.
-     It is a good idea to cover this with unit tests as well (see test/build-time-trends file)
-
-     Checking for the right configuration could be something like this:
-
-     if (!config.myrequiredConfig) {
-     return jobCallback('missing configuration properties!');
-     }
-
-
-     3. SENDING DATA BACK TO THE WIDGET
-
-     You can send data back to the widget anytime (ex: if you are hooked into a real-time data stream and
-     don't want to depend on the jobCallback triggered by the scheduler to push data to widgets)
-
-     jobWorker.pushUpdate({data: { title: config.widgetTitle, html: 'loading...' }}); // on Atlasboard > 1.0
-
-
-     4. USE OF JOB_CALLBACK
-
-     Using nodejs callback standard conventions, you should return an error or null (if success)
-     as the first parameter, and the widget's data as the second parameter.
-
-     This is an example of how to make an HTTP call to google using the easyRequest dependency,
-     and send the result to the registered widgets.
-     Have a look at test/build-time-trends for an example of how to unit tests this easily by mocking easyRequest calls
-
-     */
-
-    dependencies.easyRequest.HTML('http://google.com', function (err, html) {
-      // logger.trace(html);
-      jobCallback(err, {title: config.widgetTitle, html: html});
-    });
+      jobCallback(null, {
+        jobConfig: config,
+        title: job,
+        buildTimes
+      });
+    } catch (e) {
+      console.error(e);
+      jobCallback(e.message);
+    }
   }
 };
--- widgets/build-time-trends/build-time-trends.js
+++ widgets/build-time-trends/build-time-trends.js
@@ -1,13 +1,14 @@
 widget = {
+  createChart: function (buildTimes) {
+    return `
+        <div>${buildTimes.join(' ')}
+        </div>`;
+  },
+
   //runs when we receive data from the job
   onData: function (el, data) {
+    const { title, buildTimes } = data;
+    const content = this.createChart(buildTimes);
 
-    //The parameters our job passed through are in the data object
-    //el is our widget element, so our actions should all be relative to that
-    if (data.title) {
-      $('h2', el).text(data.title);
-    }
-
-    $('.content', el).html(data.text);
-  }
+    $('.content', el).html(content);  }
 };

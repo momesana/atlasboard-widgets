2d0c9782fddb4f3a22c1359c55a6c1a9cd9f7dad
Mehdi Salem <salem@axivion.com>
2019-02-23 14:05:15 +0100

    add ability to fetch more than just the first page of deskpro tickets

--- jobs/deskpro-tickets/deskpro-tickets.js
+++ jobs/deskpro-tickets/deskpro-tickets.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
+const assert = require('assert');
 
-const fetchTicketsWaitingForAgent = async (accessToken, baseUrl) => {
+const fetchTicketsWaitingForAgent = async (accessToken, baseUrl, page = 1) => {
 	const response = await axios({
-		url: `${baseUrl}/api/tickets?with_messages=false&with_loaded_linked_tickets=false&status=awaiting_agent&API-TOKEN=${accessToken}`,
+		url: `${baseUrl}/api/tickets?page=${page}&with_messages=false&with_loaded_linked_tickets=false&status=awaiting_agent&API-TOKEN=${accessToken}`,
 		headers: {
 			Accept: 'application/json',
 		}
@@ -11,13 +12,32 @@ const fetchTicketsWaitingForAgent = async (accessToken, baseUrl) => {
 	return response.data;
 };
 
+const fetchAll = async (accessToken, baseUrl, maximumNumberOfItems) => {
+	let tickets = [];
+	let currentPage = 0;
+
+	while (true) {
+		const result = await fetchTicketsWaitingForAgent(accessToken, baseUrl, ++currentPage);
+		const { page, per_page, total } = result;
+		assert(currentPage === page);
+		tickets = [...tickets, ...Object.values(result.tickets)];
+		const pageCount = Math.ceil(total / per_page);
+
+		if (currentPage >= pageCount || tickets.length >= maximumNumberOfItems) {
+			break;
+		}
+	}
+
+	return tickets.slice(0, maximumNumberOfItems);
+};
+
 module.exports = {
 	onInit(config, dependencies) {
 	},
 
 	async onRun(config, dependencies, jobCallback) {
 		try {
-			const { globalAuth, authName = 'deskpro' } = config;
+			const { globalAuth, authName = 'deskpro', numberOfItems = 150 } = config;
 
 			if (!globalAuth || !globalAuth[authName] || !globalAuth[authName].accessToken) {
 				return jobCallback('missing deskpro credentials');
@@ -25,8 +45,7 @@ module.exports = {
 
 			const { accessToken } = globalAuth[authName];
 			const { baseUrl } = config;
-			const result = await fetchTicketsWaitingForAgent(accessToken, baseUrl);
-			const ticketsAwaitingAgent = Object.values(result.tickets);
+			const ticketsAwaitingAgent = await fetchAll(accessToken, baseUrl, numberOfItems);
 
 			const tickets = {
 				all: ticketsAwaitingAgent,

365c1137825a0b9f13a020c4d6ead4dbb816680c
lummax <luogpg@googlemail.com>
2019-03-23 21:24:14 +0100

    build-time-trends: add ability to show multiple

--- jobs/build-time-trends/build-time-trends.js
+++ jobs/build-time-trends/build-time-trends.js
@@ -1,9 +1,30 @@
 const axios = require('axios');
 
-module.exports = {
+const fetchData = async (baseUrl, username, password, job, descriptionFilters, numberOfItems) => {
+    const response = await axios({
+        url: `${baseUrl}/job/${job}/api/json?tree=builds[description,duration,displayName,timestamp]`,
+        auth: {
+            username,
+            password
+        },
+        headers: {
+            Accept: 'application/json',
+        }
+    });
 
-	onInit(config, dependencies) {
-	},
+    return {
+        job,
+        data: response.data
+                      .builds
+                      .filter(({ description }) => description && (!Object.keys(descriptionFilters).includes(job)
+                                                                   || description.includes(descriptionFilters[job])))
+                      .map(({ duration, displayName, timestamp }) => ({ duration, displayName, timestamp }))
+                      .slice(0, numberOfItems)
+                      .reverse(),
+    };
+};
+
+module.exports = {
 
 	onRun: async function (config, dependencies, jobCallback) {
 		try {
@@ -15,29 +36,16 @@ module.exports = {
 			}
 
 			const { username, accessToken: password } = globalAuth[authName];
-			const { numberOfItems, baseUrl, job, descriptionFilter, widgetTitle} = config;
-			const response = await axios({
-				url: `${baseUrl}/job/${job}/api/json?tree=builds[description,duration,displayName,timestamp]`,
-				auth: {
-					username,
-					password
-				},
-				headers: {
-					Accept: 'application/json',
-				}
-			});
+			const { numberOfItems, baseUrl, jobs, descriptionFilters, widgetTitle} = config;
 
-			const buildTimeDetails = response.data.builds
-				.filter(({ description }) => description && description.includes(descriptionFilter))
-				.map(({ duration, displayName, timestamp }) => ({ duration, displayName, timestamp }))
-				.slice(0, numberOfItems)
-				.reverse();
+            const fetchForJob = job => fetchData(baseUrl, username, password, job, descriptionFilters, numberOfItems);
+			const buildTimeDetails = await Promise.all(jobs.map(fetchForJob));
 
 			jobCallback(null, {
-				job,
 				title: widgetTitle,
 				buildTimeDetails
 			});
+
 		} catch (e) {
 			console.error(e);
 			jobCallback(e.message);
--- widgets/build-time-trends/build-time-trends.js
+++ widgets/build-time-trends/build-time-trends.js
@@ -1,5 +1,38 @@
+
 widget = {
-	createChart: function (canvas, job, durations, labels) {
+	createChart: function (canvas, buildTimeDetails) {
+        const chartColors = [
+            'rgb(54, 162, 235)',  // blue
+            'rgb(255, 99, 132)',  // red
+            'rgb(75, 192, 192)',  // green
+            'rgb(255, 205, 86)',  // yellow
+            'rgb(153, 102, 255)', // purple
+            'rgb(255, 159, 64)',  // orange
+            'rgb(231,233,237)'    // grey
+        ];
+
+	    const colors = chartColors.map(color => ({
+          backgroundColor: Chart.helpers.color(color).alpha(0.2).rgbString(),
+          borderColor: color,
+          pointBackgroundColor: color,
+        }));
+
+        const buildDurations = (data) => data.reduce((acc, { duration, timestamp }) => {
+            acc.push({
+                x: new Date(timestamp),
+                y: duration / 3600000,
+            });
+            return acc;
+        }, []);
+
+        const buildJobData = ({job, data}, index) => Object.assign({
+            label: job,
+            fill: true,
+            borderWidth: 2,
+            pointRadius: 5,
+            data: buildDurations(data),
+        }, colors[index % colors.length]);
+
 		new Chart(canvas, {
 			type: 'line',
 			options: {
@@ -10,36 +43,26 @@ widget = {
 							display: true,
 							labelString: 'duration in hours'
 						}
+					}],
+					xAxes: [{
+					    type: 'time',
+					    time: {
+					        unit: 'day',
+					    },
 					}]
 				}
 			},
 			data: {
-				labels,
-				datasets: [{
-					label: job,
-					backgroundColor: "rgba(255,99,132,0.2)",
-					borderColor: "rgba(255,99,132,1)",
-					fill: true,
-					borderWidth: 2,
-					pointRadius: 5,
-					hoverBackgroundColor: "rgba(255,99,132,0.4)",
-					hoverBorderColor: "rgba(255,99,132,1)",
-					data: durations,
-				}]
+				datasets: buildTimeDetails.map(buildJobData),
 			}
 		});
 	},
 
 	onData: function (el, data) {
 		const { job, title, buildTimeDetails } = data;
-		$('h2.widget-title', el).text(`${title}: ${job}`);
+		$('h2.widget-title', el).text(`${title}`);
 		const canvas = $('canvas', el);
-		const { durations, labels } = buildTimeDetails.reduce((acc, { duration, displayName, timestamp }) => {
-			acc.durations.push(duration / 3600000);
-			acc.labels.push(dateFormat(new Date(timestamp), `ddd dd.mm`));
-			return acc;
-		}, { durations: [], labels: [] });
-		this.createChart(canvas, job, durations, labels);
+		this.createChart(canvas, buildTimeDetails);
 	}
 };
 

1aad9a1aa5e70fc94ebd1d80ab0a409786fa9685
Mehdi Salem <momesana@gmail.com>
2025-08-04 19:14:58 +0200

    make content of jenkins-job-matrix scrollable

--- widgets/jenkins-job-matrix/jenkins-job-matrix.js
+++ widgets/jenkins-job-matrix/jenkins-job-matrix.js
@@ -1,46 +1,50 @@
 widget = {
 	createMatrix({ subtitle, runResults, jobUrl }) {
-		const rows = Object.entries(runResults).map(item => this.createRow(item, jobUrl));
+		const rows = Object.entries(runResults).map((item) =>
+			this.createRow(item, jobUrl),
+		);
 		content = rows.length
-		? `
+			? `
 			<div class="job-matrix">
-			${rows.join('')}
+			${rows.join("")}
 			</div>
 		`
-		: `<div class="idle">Idle</div>`;
-		return $(`
-			<h2 class="widget-title">
-				<a href="${jobUrl}" target="_blank">${subtitle}</a>
-			</h2>
-			${content}
-		`);
+			: `<div class="idle">Idle</div>`;
+		return $(content);
 	},
 
 	createRow([label, data], jobUrl) {
 		console.log(data);
-		const items = data.map(item => `
+		const items = data.map(
+			(item) => `
 			<a title="${item.fullDisplayName}"
 			   href="${jobUrl}/${item.id}"
 			   target=”_blank”
 			   class="job-matrix-bubbles-item ${item.value}"
 			/>
-		`);
-		const os = label.includes('win')
-			? 'windows'
-			: label.includes('macos')
-			? 'apple'
-			: 'linux';
+		`,
+		);
+		const os = label.includes("win")
+			? "windows"
+			: label.includes("macos")
+				? "apple"
+				: "linux";
 		return `
 			<div class="job-matrix-label">
 				<span>${label}</span>
 				<span style="margin-left: 0.25em;"><i class="fab fa-${os}"/></span>
 			</div>
-			<div class="job-matrix-bubbles">${items.join('')}</div>
+			<div class="job-matrix-bubbles">${items.join("")}</div>
 		`;
 	},
 	//runs when we receive data from the job
 	onData(el, data) {
-		console.log(data);
-		$('.content', el).html(this.createMatrix(data));
-	}
+		const { subtitle, runResults, jobUrl } = data;
+		$(".widget-title").html(`
+			<h2 class="widget-title">
+				<a href="${jobUrl}" target="_blank">${subtitle}</a>
+			</h2>
+		`);
+		$(".content", el).html(this.createMatrix({ runResults, jobUrl }));
+	},
 };
